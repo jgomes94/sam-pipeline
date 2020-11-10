dbd618600e8eb8769d8f1b75b2ad0b49de78d17d
João <gomes.jfn@gmail.com>
2020-11-10 16:37:27 +0000

    app change in code to validate if the template works

--- hello-world/app.js
+++ hello-world/app.js
@@ -16,7 +16,7 @@ let response;
  */
 exports.lambdaHandler = async (event, context) => {
     try {
-        // const ret = await axios(url);
+        console.log('hi does this work?');
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
