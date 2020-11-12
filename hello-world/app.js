73c5a1e41080cb5eda6cfc97cc941bef97907aa9
João <gomes.jfn@gmail.com>
2020-11-12 16:56:05 +0000

    remove console log

--- hello-world/app.js
+++ hello-world/app.js
@@ -2,12 +2,6 @@
 let response;
 
 exports.lambdaHandler = async (event, context) => {
-    console.log('testing2');
-    console.log('testing3');
-    console.log('testing4');
-    console.log('testing5');
-    console.log('testing6');
-    console.log('testing7');
 
     try {
         response = {
