46a4ca006e4f06544bae1d0d978b685e95993386
João <gomes.jfn@gmail.com>
2020-11-13 16:03:51 +0000

    remove this is me mario

--- hello-world/app.js
+++ hello-world/app.js
@@ -2,10 +2,6 @@
 let response;
 
 exports.lambdaHandler = async (event, context) => {
-    console.log('hello this me, mario!!')
-    console.log('hello this me, mario!!')
-    console.log('hello this me, mario!!')
-    console.log('hello this me, mario!!')
 
     try {
         response = {
