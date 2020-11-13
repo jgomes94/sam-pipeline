3fc319acefbd7fdd9acf6c76eaa317b708714a9a
João <gomes.jfn@gmail.com>
2020-11-13 16:00:20 +0000

    lets take this boy for a spin

--- hello-world/app.js
+++ hello-world/app.js
@@ -4,6 +4,8 @@ let response;
 exports.lambdaHandler = async (event, context) => {
     console.log('hello this me, mario!!')
     console.log('hello this me, mario!!')
+    console.log('hello this me, mario!!')
+    console.log('hello this me, mario!!')
 
     try {
         response = {
